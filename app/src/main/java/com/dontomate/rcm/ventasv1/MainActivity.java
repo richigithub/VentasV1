9dfa224cd6888d43402f2e63ded14a19b41d4539
Ricardo Cerinza Martínez <rcerinza@hotmail.com>
2017-07-28 23:28:05 -0500

    DB Helpers and DBManager Writer Succesfull (do not date  and time in the  table sale).

--- app/src/main/java/com/dontomate/rcm/ventasv1/MainActivity.java
+++ app/src/main/java/com/dontomate/rcm/ventasv1/MainActivity.java
@@ -1,13 +1,30 @@
 package com.dontomate.rcm.ventasv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnVentas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //obteniendo una instancia del boton btnVentas
+        btnVentas = (Button)findViewById(R.id.btn_ver_ventas);
+        //Registrando la escucha sobre la actividad Main
+        btnVentas.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentVentas =  new Intent(MainActivity.this, AgregarVenta.class);
+                startActivity(intentVentas);
+            }
+        });
     }
+
 }
